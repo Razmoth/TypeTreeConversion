0ac9048440802a5f4c1779b10973a526dbf5f3f7
Razmoth <32140579+Razmoth@users.noreply.github.com>
2023-09-10 22:31:39 +0400

    New type

---
+++ GI_2.4/Classes/MiHoYoGrassLand/FieldConverter.cs
@@ -0,0 +1,21 @@
+﻿using AssetsTools.NET;
+
+namespace TypeTreeConversion.MiHoYoGrassLand;
+
+public class FieldConverter : DefaultFieldConverter
+{
+	public FieldConverter(ClassDatabaseFile classDatabase) : base(classDatabase)
+	{
+	}
+
+	protected override AssetTypeValueField? CreateNewBaseField(int originalTypeID)
+	{
+		return base.CreateNewBaseField(218);
+	}
+
+	protected override void CopyFields(UnityAsset asset, AssetTypeValueField source, AssetTypeValueField destination)
+	{
+		CopyFieldsExactly(source, destination);
+		CopyFieldsExactly(source["m_GrassData"], destination["m_TerrainData"]);
+	}
+}
--- GI_2.4/Plugin.cs
+++ GI_2.4/Plugin.cs
@@ -16,6 +16,7 @@ public class Plugin : ConversionPlugin
 		registry.Converters.Add(ClassIDType.MiHoYoBinData, new MiHoYoBinData.FieldConverter(registry.ClassDatabase, indexObjectConverter));
 		registry.Converters.Add(ClassIDType.MiHoYoGrassBlock, new MiHoYoGrassBlock.FieldConverter(registry.ClassDatabase));
 		registry.Converters.Add(ClassIDType.MiHoYoGrassData, new MiHoYoGrassData.FieldConverter(registry.ClassDatabase));
+		registry.Converters.Add(ClassIDType.MiHoYoGrassLand, new MiHoYoGrassLand.FieldConverter(registry.ClassDatabase));
 		registry.Converters.Add(ClassIDType.NavMeshHeightFieldData, new NavMeshHeightFieldData.FieldConverter(registry.ClassDatabase));
 		registry.DefaultConverter = new DefaultFieldConverter(registry.ClassDatabase);
 	}
@@ -27,6 +28,7 @@ public class Plugin : ConversionPlugin
 		registry.Replacers.Add(ClassIDType.IndexObject, new IndexObject.TypeTreeReplacer(registry.ClassDatabase));
 		registry.Replacers.Add(ClassIDType.MiHoYoGrassBlock, new MiHoYoGrassBlock.TypeTreeReplacer(registry.ClassDatabase));
 		registry.Replacers.Add(ClassIDType.MiHoYoGrassData, new MiHoYoGrassData.TypeTreeReplacer(registry.ClassDatabase));
+		registry.Replacers.Add(ClassIDType.MiHoYoGrassLand, new MiHoYoGrassLand.TypeTreeReplacer(registry.ClassDatabase));
 		registry.Replacers.Add(ClassIDType.NavMeshHeightFieldData, new NavMeshHeightFieldData.TypeTreeReplacer(registry.ClassDatabase));
 		registry.DefaultReplacer = new DefaultTypeTreeReplacer(registry.ClassDatabase);
 	}
